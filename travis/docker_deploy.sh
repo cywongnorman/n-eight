376b45360bd6022598237ada59542c70d3e5595c
cywongnorman <cywongnorman@gmail.com>
2019-06-13 22:51:06 +0800

    revoke travis

--- travis/docker_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push cywongnorman/n-eight:$TRAVIS_BRANCH
-if [ "$TRAVIS_BRANCH" = "master" ]; then docker push cywongnorman/n-eight:latest; fi
